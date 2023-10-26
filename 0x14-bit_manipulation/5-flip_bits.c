fd7ecf7ddb140b578ad8407ea4bc2579cca9281e
Hajar <132363063+Hajar-developer@users.noreply.github.com>
2023-10-26 23:07:01 +0100

    0x14. C - Bit manipulation

--- 0x14-bit_manipulation/5-flip_bits.c
+++ 0x14-bit_manipulation/5-flip_bits.c
@@ -10,7 +10,7 @@
  */
 unsigned int flip_bits(unsigned long int n, unsigned long int m)
 {
-	int i, cnt = 0;
+	int i, count = 0;
 	unsigned long int current;
 	unsigned long int exclusive = n ^ m;
 
@@ -21,5 +21,5 @@ unsigned int flip_bits(unsigned long int n, unsigned long int m)
 			count++;
 	}
 
-	return (cnt);
+	return (count);
 }
